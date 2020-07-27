d716f01479eb4f463b06d7664769b0d9d9855866
Darpan <darpan@proclink.com>
2020-07-27 05:46:00 +0530

    profile details and side buttons

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pinkvilla/base/app_video_player.dart';
+import 'package:pinkvilla/base/spinner_animation.dart';
 import 'package:pinkvilla/bloc/bloc.dart';
+import 'package:pinkvilla/config/assets.dart';
+import 'package:pinkvilla/config/dimen.dart';
 import 'package:pinkvilla/repos/models/video.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -48,7 +51,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         AppVideoPlayer(
                           url: videoDetail.url,
                         ),
-                        /*  onScreenControls(),*/
+                        onScreenControls(videoDetail),
                       ],
                     ),
                   );
@@ -60,6 +63,196 @@ class _HomeScreenState extends State<HomeScreen> {
         }
         return Container();
       },
+    );
+  }
+
+  Widget onScreenControls(Video videoDetail) {
+    return Container(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            flex: 5,
+            child: videoDesc(videoDetail),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: EdgeInsets.only(bottom: 60, right: 0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  userProfile(videoDetail),
+                  videoControlAction(
+                    icon: AppIcons.heart,
+                    label: '${videoDetail.likeCount}',
+                  ),
+                  videoControlAction(
+                    icon: AppIcons.chat_bubble,
+                    label: '${videoDetail.commentCount}',
+                  ),
+                  videoControlAction(
+                    icon: AppIcons.reply,
+                    label: '${videoDetail.shareCount}',
+                    size: 27,
+                  ),
+                  SpinnerAnimation(
+                    body: audioSpinner(),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget videoDesc(Video videoDetail) {
+    return Container(
+      padding: EdgeInsets.only(left: 16, bottom: 60),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(top: 7, bottom: 7),
+            child: Text(
+              '@${videoDetail.user.name}',
+              style: TextStyle(
+                fontSize: 14,
+                color: Colors.white,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 4, bottom: 7),
+            child: Text(
+              '${videoDetail.title}',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 14,
+                fontWeight: FontWeight.w300,
+              ),
+            ),
+          ),
+          Row(
+            children: <Widget>[
+              Icon(
+                Icons.music_note,
+                size: 19,
+                color: Colors.white,
+              ),
+              Expanded(
+                child: Text(
+                  '${videoDetail.user.name}',
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 14,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget userProfile(Video videoDetail) {
+    return Padding(
+      padding: EdgeInsets.only(top: 10, bottom: 10),
+      child: Column(
+        children: <Widget>[
+          Stack(
+            alignment: AlignmentDirectional.center,
+            children: <Widget>[
+              Container(
+                height: 50,
+                width: 50,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.white,
+                    width: 1.0,
+                    style: BorderStyle.solid,
+                  ),
+                  image: DecorationImage(
+                    image: NetworkImage(videoDetail.user.headshot),
+                  ),
+                  shape: BoxShape.circle,
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 50),
+                height: 18,
+                width: 18,
+                child: Icon(
+                  Icons.add,
+                  size: 10,
+                  color: Colors.white,
+                ),
+                decoration: BoxDecoration(
+                  color: Color.fromRGBO(255, 42, 84, 1),
+                  shape: BoxShape.circle,
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget videoControlAction({IconData icon, String label, double size = 35}) {
+    return Padding(
+      padding: EdgeInsets.only(top: 10, bottom: 10),
+      child: Column(
+        children: <Widget>[
+          Icon(
+            icon,
+            color: Colors.white,
+            size: size,
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+                top: Dimen.defaultTextSpacing,
+                bottom: Dimen.defaultTextSpacing),
+            child: Text(
+              label ?? "",
+              style: TextStyle(fontSize: 10, color: Colors.white),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget audioSpinner() {
+    return Container(
+      width: 50.0,
+      height: 50.0,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Colors.grey[800],
+            Colors.grey[900],
+            Colors.grey[900],
+            Colors.grey[800]
+          ],
+          stops: [0.0, 0.4, 0.6, 1.0],
+          begin: Alignment.bottomLeft,
+          end: Alignment.topRight,
+        ),
+        shape: BoxShape.circle,
+        image: DecorationImage(
+          image: AssetImage("assets/images/avatar.png"),
+        ),
+      ),
     );
   }
 }
